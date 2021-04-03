82a60a6cdf73c2ae56116b3c73f848bc034352f3
WQ7-JS <63476262+WQ7-JS@users.noreply.github.com>
2021-04-03 19:47:54 +0200

    Delete postlabel.js

--- postlabel.js
+++
@@ -1,4 +0,0 @@
-var dhar = &#39;<data:label.name/>&#39;;
-if(dhar == &quot;DogsTubexw&quot; || dhar == &quot;label2&quot;){
-document.write(&quot;<div class='DogsTubexw-climb'/>&quot;);
-flag=flag*0;}
